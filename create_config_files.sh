6d2b165d5d5ab50a9fe876566a312d168d2a24a3
inclemenstv <inclemenstv@gmail.com>
2021-06-02 11:00:45 +0300

    fix gitignore

--- create_config_files.sh
+++ create_config_files.sh
@@ -5,4 +5,3 @@ envsubst < job_config.xml.example > job_config.xml
 envsubst < Vagrantfile.example > Vagrantfile
 envsubst < ./groovy_scripts/init_script.groovy.example > ./groovy_scripts/init_script.groovy
 envsubst < credential.xml.example > credential.xml
-envsubst < Jenkinsfile.example > Jenkinsfile
