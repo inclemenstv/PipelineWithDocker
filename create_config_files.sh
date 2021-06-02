14208b520603c770c6a9e6cec2a9877d0154bd46
inclemenstv <inclemenstv@gmail.com>
2021-06-02 14:53:18 +0300

    change sh

--- create_config_files.sh
+++ create_config_files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source .env
 
-envsubst < job_config.xml.example > job_config.xml
-envsubst < Vagrantfile.example > Vagrantfile
-envsubst < ./groovy_scripts/init_script.groovy.example > ./groovy_scripts/init_script.groovy
-envsubst < credential.xml.example > credential.xml
+envsubst < job_config.xml.template > job_config.xml
+envsubst < Vagrantfile.template > Vagrantfile
+envsubst < ./groovy_scripts/init_script.groovy.template > ./groovy_scripts/init_script.groovy
+envsubst < credential.xml.template > credential.xml
